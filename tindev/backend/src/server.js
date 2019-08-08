ac99ef7872ef02353c0bb64097af5fb27a9e3b22
Marcelo Gasparetti <mgasparetti86@gmail.com>
2019-08-07 23:13:45 -0300

    Backend application coded

--- tindev/backend/src/server.js
+++ tindev/backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose =  require('mongoose');
+const cors =  require('cors');
+
 const routes = require('./routes');
 
 const server = express();
@@ -7,6 +9,8 @@ const server = express();
 mongoose.connect('mongodb+srv://tindev:tindev01@cluster0-xghod.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true
 });
+
+server.use(cors());
 server.use(express.json()); 
 server.use(routes);
 
